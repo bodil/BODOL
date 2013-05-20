69258de76d28cb0a28cd62406c6bdd96b7031f25
Bodil Stokke <bodil@bodil.tv>
2013-05-20 18:35:40 +0200

    Generalise arguments for state monad functions.

--- src/bodol/monad.clj
+++ src/bodol/monad.clj
@@ -3,21 +3,21 @@
 (defn reduce-state
   "Evaluate a list of state monad values in sequence, using the
    provided initial state."
-  [scope mvs]
+  [state mvs]
   (reduce (fn [[value state] next]
             (next state))
-          [nil scope]
+          [nil state]
           mvs))
 
 (defn map-state
   "Evaluate a list of state monad values in sequence, producing
    a list of the results of each evaluation."
-  [scope mvs]
+  [state mvs]
   (reduce (fn [[values state] next]
             (let [[value state] (next state)]
               [(conj values value) state]))
-          [[] scope]
+          [[] state]
           mvs))
 
-(defn state-id [scope]
-  [nil scope])
+(defn state-id [state]
+  [nil state])
